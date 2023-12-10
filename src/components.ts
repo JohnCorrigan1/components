ec9dbb21cdd2529092eebfdd96591ef03a23bdbf
John Corrigan <jcorrigan322@gmail.com>
2023-12-09 18:22:51 -0800

    component categories'

--- src/components.ts
+++ src/components.ts
@@ -7,6 +7,33 @@ export type ComponentData = {
   hasAnimation: boolean;
 };
 
+export type ComponentCategory = {
+  name: string;
+  link: string;
+};
+
+export const cardComponents: ComponentCategory[] = [
+  {
+    name: "Card",
+    link: "/components/card",
+  },
+  {
+    name: "Full Image Card",
+    link: "/components/full-image-card",
+  },
+];
+
+export const navComponents: ComponentCategory[] = [
+  {
+    name: "Navbar",
+    link: "/components/navbar",
+  },
+  {
+    name: "Sidebar",
+    link: "/components/sidebar",
+  },
+];
+
 export const componentData: ComponentData[] = [
   {
     name: "Navbar",
@@ -56,12 +83,42 @@ export const componentData: ComponentData[] = [
   {
     name: "Card",
     link: "/components/card",
-    code: `<div class="some classes will go here tailwind">
-                <h1>Card</h1>
-                <p>Some text</p>
-             </div>`,
+    code: `
+    <div class="flex flex-col overflow-hidden max-w-xl items-center bg-slate-700
+     bg-opacity-60 hover:bg-opacity-40 cursor-pointer shadow-lg drop-shadow-lg rounded-lg duration-300">
+        <img class="w-full h-3/4 rounded-t-lg"
+          src=""
+          alt="image"
+        />
+        <div class="flex w-full h-1/4 p-8 items-center justify-center">
+            <p class="text-white font-semibold">wow such a nice image</p>
+        </div>
+    </div>
+    `,
     category: "Images",
     description: "A simple card component.",
+    hasAnimation: false,
+  },
+  {
+    name: "Full Image Card",
+    link: "/components/full-image-card",
+    code: `
+    <a href="">
+        <div class="relative duration-500 cursor-pointer text-white rounded-lg w-full">
+            <div>
+                <img src="" alt="image" class="rounded-lg" />
+                <h1 class="font-semibold text-xl absolute bottom-10 w-full text-center">
+                    Cool picture
+                </h1>
+            </div>
+            <div class="absolute bottom-5 w-full flex flex-col items-center justify-center text-white">
+                <p>That picture is really nice good stuff man</p>
+            </div>
+        </div>
+    </a>
+`,
+    category: "Images",
+    description: "A simple full image card component.",
     hasAnimation: false,
   },
 ];
